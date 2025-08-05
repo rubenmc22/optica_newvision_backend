042dd35adb7bc280e962917a5e4aa39b16f11a23
NOMBRE <64760106+JeffersonTorres77@users.noreply.github.com>
2025-08-04 23:26:26 -0400

    Update SedesController.js

--- src/controllers/SedesController.js
+++ src/controllers/SedesController.js
@@ -4,8 +4,16 @@ const SedesController = {
     get: async (req, res) => {
         try {
             const sedes = await Sede.findAll();
+
+            let sedes_output = [];
+            for(const sede of sedes) {
+                sedes_output.push({
+                    key: sede.id,
+                    nombre: sede.nombre
+                });
+            }
             
-            res.status(200).json({ message: 'ok', sedes: sedes });
+            res.status(200).json({ message: 'ok', sedes: sedes_output });
         } catch (err) {
             console.error(err);
             res.status(400).json(err);
