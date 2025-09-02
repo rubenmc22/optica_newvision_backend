8c310477e8082dd33fc3de89791f034477300c47
NOMBRE <64760106+JeffersonTorres77@users.noreply.github.com>
2025-09-02 00:02:45 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const { testConnection } = require('./config/db');
 const cors = require('cors');
 const Usuario = require('./models/Usuario');
 const path = require('path');
+const HashUtils = require('./utils/HashUtil');
+const Paciente = require('./models/Paciente');
 
 require('./crons/RastrearBcv')();
 
@@ -24,6 +26,32 @@ app.use('/public', express.static(path.join(__dirname, '../public')));
 // Rutas
 app.get('/', async (req, res) => {
   res.json({ message: "Hello world: 2025-08-18" });
+});
+
+app.get('/ajustar-id-pacintes', async (req, res) => {
+  let countPacientes = 0;
+  let countHistorias = 0;
+
+  const pacientes = await Paciente.findAll();
+  for(let paciente of pacientes) {
+    countPacientes++;
+
+    const pkey = paciente.pkey;
+    const new_pkey = HashUtils.generate(paciente.id);
+    
+    paciente.pkey = new_pkey;
+    paciente.save();
+
+    const historias = await HistorialMedico.findAll({ where: { paciente_id: pkey } });
+    for(let historia of historias) {
+      countHistorias++;
+
+      historia.paciente_id = new_pkey;
+      historia.save();
+    }
+  }
+
+  res.json({ message: "OK", total_pacientes: countPacientes, total_historias: countHistorias });
 });
 
 app.use('/api', routes);
